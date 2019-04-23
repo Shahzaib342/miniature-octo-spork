610a6cc1639a70081c86ab6e2327eff275fc803a
Mohsin Ali <alimohsin7719@gmail.com>
2019-04-23 23:06:40 +0500

    add php file

--- form.js
+++ form.js
@@ -16,3 +16,21 @@ function myFunction(variab) {
   $('#'+variab+'-menu').addClass('active-border')
   $('#'+variab+'-tick').css("visibility", "visible")
 }
+
+$("#idForm").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var url = form.attr('action');
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: form.serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               alert(data); // show response from the php script.
+           }
+         });
+        });
